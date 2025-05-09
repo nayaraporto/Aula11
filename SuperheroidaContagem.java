7bfc393252a58a7acfa73b14ccd653889748cd0d
nayaraporto <naya08.vitoria@gmail.com>
2025-05-09 14:32:41 -0300

    Update SuperheroidaContagem.java

--- SuperheroidaContagem.java
+++ SuperheroidaContagem.java
@@ -1,10 +1,40 @@
 package Aula11;
-
+import java.util.Scanner;
 public class SuperheroidaContagem {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner ler = new Scanner (System.in);
+		
+		System.out.println("Digite o número até qual você que contar");
+		int numeroFinal = ler.nextInt();
+		int contador = 1;
+		
+		do {
+			System.out.println("Contando: " + contador);
 
+		if (contador % 10 == 0) {
+			System.out.print("ARMADILHA Digite o código da armadilha para continuar: ");
+			String codigo = ler.next();
+			if (!codigo.equals("1234")) {
+				System.out.println("Código errado, Você falhou");
+				break;
+			}
+		}
+		contador++;
+		}while (contador <= numeroFinal);
+		
+		if (contador > numeroFinal) {
+			System.out.println("Você salvou a cidade");
+		}
+		
+		ler.close();
+		
+		
+		
+		
+		
+		
 	}
 
 }
