1975d80dc14ac3b3601c3beca9cde3a2e4f60e5d
nayaraporto <naya08.vitoria@gmail.com>
2025-05-09 14:33:06 -0300

    Update GabaritoProva.java

--- GabaritoProva.java
+++ GabaritoProva.java
@@ -1,9 +1,45 @@
 package Aula11;
-
+import java.util.Scanner;
 public class GabaritoProva {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner ler = new Scanner(System.in);
+		String[] gabarito = {"A", "B", "C", "D", "E", "E", "D", "C", "B", "A"};
+		int totalAlunos = 0;
+		int somaNotas = 0;
+		String continuar;
+
+		do {
+		totalAlunos++;
+		int acertos = 0;
+		String[] respostasAluno = new String[10];
+
+		System.out.println("Responda as 10 questões (A, B, C, D ou E):");
+
+		for (int i = 0; i < 10; i++) {
+		System.out.print("Questão " + (i + 1) + ": ");
+		respostasAluno[i] = ler.next();
+
+		if (respostasAluno[i].equalsIgnoreCase(gabarito[i])) {
+		acertos++;
+		}
+		}
+
+		System.out.println("Você acertou " + acertos + " questões.");
+		somaNotas += acertos;
+
+		System.out.print("Outro aluno vai utilizar o sistema? (S/N): ");
+		continuar = ler.next();
+
+		} while (continuar.equalsIgnoreCase("S"));
+
+		double media = (double) somaNotas / totalAlunos;
+		System.out.println("\nTotal de alunos: " + totalAlunos);
+		System.out.println("Média da turma: " + media);
+
+		ler.close();
+		}
+		{
 
 	}
 
